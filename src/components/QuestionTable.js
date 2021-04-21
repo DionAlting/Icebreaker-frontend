30087a0d44b2b3ed278914f2925d105c8c0a204e
Dion Alting <dion_a@live.nl>
2021-04-21 17:38:50 +0200

    Added a header

--- src/components/QuestionTable.js
+++ src/components/QuestionTable.js
@@ -10,47 +10,53 @@ const QuestionTable = () => {
   useEffect(() => {
     dispatch(FetchQuestions());
   }, [dispatch]);
+
   return (
-    <table class="table p-4 bg-white shadow rounded-lg w-full">
-      <thead>
-        <tr>
-          <th class="border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-normal text-gray-900">
-            #
-          </th>
-          <th class="border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-normal text-gray-900">
-            Question
-          </th>
-          <th class="border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-normal text-gray-900">
-            Target Number
-          </th>
-          <th class="border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-normal text-gray-900">
-            Group Name
-          </th>
-          <th class="border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-normal text-gray-900">
-            Answered
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        {allQuestions.map((question) => (
-          <tr class="text-gray-700" key={question.id}>
-            <td class="border-b-2 p-4 dark:border-dark-5">{question.id}</td>
-            <td class="border-b-2 p-4 dark:border-dark-5">
-              {question.question}
-            </td>
-            <td class="border-b-2 p-4 dark:border-dark-5">
-              {question.targetNumber}
-            </td>
-            <td class="border-b-2 p-4 dark:border-dark-5">
-              {question.groupName}
-            </td>
-            <td class="border-b-2 p-4 dark:border-dark-5">
-              <button className="bg-red-400">Answered</button>
-            </td>
+    <>
+      <div className="text-center">
+        <h1 className="text-gray-800">Questions</h1>
+      </div>
+      <table class="table p-4 bg-white shadow rounded-lg w-full">
+        <thead>
+          <tr>
+            <th class="border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-normal text-gray-900">
+              #
+            </th>
+            <th class="border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-normal text-gray-900">
+              Question
+            </th>
+            <th class="border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-normal text-gray-900">
+              Target Number
+            </th>
+            <th class="border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-normal text-gray-900">
+              Group Name
+            </th>
+            <th class="border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-normal text-gray-900">
+              Answered
+            </th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {allQuestions.map((question) => (
+            <tr class="text-gray-700" key={question.id}>
+              <td class="border-b-2 p-4 dark:border-dark-5">{question.id}</td>
+              <td class="border-b-2 p-4 dark:border-dark-5">
+                {question.question}
+              </td>
+              <td class="border-b-2 p-4 dark:border-dark-5">
+                {question.targetNumber}
+              </td>
+              <td class="border-b-2 p-4 dark:border-dark-5">
+                {question.groupName}
+              </td>
+              <td class="border-b-2 p-4 dark:border-dark-5">
+                <button className="bg-red-400">Answered</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 };
 
