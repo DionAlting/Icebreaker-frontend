509258ad801dfe9142bfb2e52237e900f5844249
Dion Alting <dion_a@live.nl>
2021-04-21 17:37:52 +0200

    Added values to input fields

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -35,18 +35,21 @@ const QuestionForm = () => {
           className="w-full px-4 py-2 mt-4 leading-tight text-gray-700 bg-gray-200 border-2 border-gray-200 rounded appearance-none focus:outline-none focus:bg-white focus:border-purple-500"
           type="text"
           placeholder="Question"
+          value={question}
           onChange={(event) => setQuestion(event.target.value)}
         />
         <input
           className="w-full px-4 py-2 mt-4 leading-tight text-gray-700 bg-gray-200 border-2 border-gray-200 rounded appearance-none focus:outline-none focus:bg-white focus:border-purple-500"
           type="text"
           placeholder="Group Name"
+          value={groupName}
           onChange={(event) => setGroupName(event.target.value)}
         />
         <input
           className="w-full px-4 py-2 mt-4 leading-tight text-gray-700 bg-gray-200 border-2 border-gray-200 rounded appearance-none focus:outline-none focus:bg-white focus:border-purple-500"
           type="text"
           placeholder="Secret Number"
+          value={targetNumber}
           onChange={(event) => setTargetNumber(event.target.value)}
         />
 
