1ea0a510178eff4c129d424fcb04ab4fc6b241f1
Giampiero90-dev <giampiero.devita@hotmail.it>
2021-04-22 16:19:26 +0200

    Added chart element for YES and NO

---
+++ src/components/Chart.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+import { PieChart } from "react-minimal-pie-chart";
+import io from "socket.io-client";
+
+const socket = io("http://localhost:4001");
+
+const Chart = () => {
+  const [count, setCount] = useState(null);
+  const shiftSize = 7;
+  function onMessage() {
+    socket.on("answer_count", (count) => {
+      console.log(count);
+      setCount(count);
+    });
+    socket.on("new_question_for_user", (question) => {
+      setCount(null);
+    });
+  }
+
+  useEffect(onMessage, []);
+
+  if (!count) return null;
+  return (
+    <PieChart
+      animate
+      data={[
+        { title: "Yes", value: count?.yes, color: "#34D399" },
+        { title: "No", value: count?.no, color: "#EF4444" },
+      ]}
+      label={({ dataEntry }) => dataEntry.value}
+      radius={PieChart.defaultProps.radius - shiftSize}
+      labelStyle={{ fill: "#ffffff" }}
+    />
+  );
+};
+
+export default Chart;
