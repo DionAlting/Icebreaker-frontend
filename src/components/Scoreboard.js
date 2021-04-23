3919f28f117569126a09ae32c4d63267abb9dba8
YOURGITHUBUSERNAME <YOUR@EMAIL.com>
2021-04-23 11:44:20 +0200

    scoreboard setup

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -1,7 +1,33 @@
 import React from "react";
 
 const Scoreboard = () => {
-  return <div>scoreboard</div>;
+  //TODO: Fix the scoreboard.
+  return (
+    <table className="table p-4 bg-white shadow rounded-lg w-full">
+      <thead>
+        <tr>
+          <th className="border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-normal text-gray-900">
+            Question
+          </th>
+          <th className="border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-normal text-gray-900">
+            Target Number
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        {/* {questions?.map((question) => (
+          <tr className="text-gray-700" key={question.id}>
+            <td className="border-b-2 p-4 dark:border-dark-5">
+              {question.question}
+            </td>
+            <td className="border-b-2 p-4 dark:border-dark-5">
+              {question.targetNumber}
+            </td>
+          </tr>
+        ))} */}
+      </tbody>
+    </table>
+  );
 };
 
 export default Scoreboard;
